c907b7a21e3fe98628bbf1508ea7021367201e5e
Ryan <74019611+Ryan-McKee2001@users.noreply.github.com>
2022-02-21 00:32:19 +0000

    updates to the reading of the file and aspect ratio

--- Feature engineering calculations script.R
+++ Feature engineering calculations script.R
@@ -86,6 +86,7 @@
   }
 
   # this gets the aspect ratio width/height
+  # need to validate aspect ratios are correct a1 calculation and function does not seem to be right
   aspect_ratio <- function(current_file_image_matrix){
     # get the pixel distance between the top most black pixel and bottom
     row_sum_matrix <- rowSums(current_file_image_matrix)
@@ -113,10 +114,41 @@
     }
     
     height <- bottom_element - top_element
+    
+    # getting the width
+    left_most_element <- 0
+    right_most_element <- 0
+    
+    col_sum_matrix <- colSums(current_file_image_matrix)
+    
+    current_index <- 1
+    while(left_most_element == 0){
+      if(col_sum_matrix[current_index] > 0){
+        left_most_element <- current_index
+        break
+      }
+      current_index <- current_index + 1
+    }
+    
+    # get the pixel index of the bottom most black pixel
+    current_index <- length(col_sum_matrix)
+    
+    while(right_most_element == 0){
+      if(col_sum_matrix[current_index] > 0){
+        right_most_element <- current_index
+        break
+      }
+      current_index <- current_index - 1
+    }
+    
+    width <- right_most_element - left_most_element
+    
+    return(width/height)
+    
   }
   
   neigh_1 <- function(current_file){
-  
+    
   }
   
   no_neigh_above <- function(current_file){
@@ -161,24 +193,22 @@
     current_file_name <- data_folder[current_index]
     current_file_path <- paste(folder_path, current_file_name, sep = "")
     
-    current_file <- read.csv(file = current_file_path, header = T)
-    
-    print(current_file)
-    
-    current_file_image_matrix <- as.matrix(current_file, nrow = 18, ncol = 19)
+    current_file <- as.matrix(read.table(file = current_file_path, header = F, sep = ","))
     
     calculated_features = matrix(ncol = 18, nrow = 1)
-    
-    print(current_file_image_matrix)
+    colnames(calculated_features) <- c("Label", "Index", "nr_pix", "rows_with_1", "cols_with_1",
+                                       "rows_with_3p", "cols_with_3p", "aspect ratio", "neigh_1",
+                                       "no_neigh_above","no_neigh_below","no_neigh_left","no_neigh_right",
+                                       "no_neigh_horiz", "no_neigh_vert","connected_areas","eyes","custom")
     
     calculated_features[1,1] <- getFileLabel(current_file_name)
     calculated_features[1,2] <- current_index
     calculated_features[1,3] <- nr_pix(current_file_image_matrix)
-    calculated_features[1,4] <- rows_with_1(current_file_image_matrix)
-    calculated_features[1,5] <- cols_with_1(current_file_image_matrix)
-    calculated_features[1,6] <- rows_with_3p(current_file_image_matrix)
-    calculated_features[1,7] <- rows_with_3p(current_file_image_matrix)
-    calculated_features[1,8] <- aspect_ratio(current_file_image_matrix)
+    calculated_features[1,4] <- rows_with_1(current_file)
+    calculated_features[1,5] <- cols_with_1(current_file)
+    calculated_features[1,6] <- rows_with_3p(current_file)
+    calculated_features[1,7] <- cols_with_3p(current_file)
+    calculated_features[1,8] <- aspect_ratio(current_file)
     #calculated_features[1,9]
     #calculated_features[1,10]
     #calculated_features[1,11]
@@ -189,6 +219,8 @@
     #calculated_features[1,16]
     #calculated_features[1,17]
     #calculated_features[1,18]
+    
+    print(calculated_features)
   }
 }
   
