a95e34dda8669069c5ba888ca9833fbd340fc068
Ryan <74019611+Ryan-McKee2001@users.noreply.github.com>
2022-03-07 19:28:27 +0000

    update

--- report/section1_code.r
+++
@@ -1,6 +0,0 @@
-# A function to print squares of a sequence of numbers up to the given number `a`.
-squarenumbers <- function(a) {
-   squares <- sapply(1:a, function(x) x^2)
-}	
-
-print(squarenumbers(5))-
