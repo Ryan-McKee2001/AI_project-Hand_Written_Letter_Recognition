d031fada2c78a3a309a5bbff878a8b3817d75c4e
Ryan <74019611+Ryan-McKee2001@users.noreply.github.com>
2022-03-07 19:22:25 +0000

    update

--- section3_code.R
+++ section3_code.R
@@ -14,12 +14,12 @@
   
   
   # need to use ggplot to make each of these histograms better.
-  nr_pix_hist <- hist(calculated_features$nr_pix)
-  row_with_1_hist <- hist(calculated_features$rows_with_1)
-  cols_with_1_hist <- hist(calculated_features$cols_with_1)
-  rows_with_3p_hist <- hist(calculated_features$rows_with_3p)
-  cols_with_3p_hist <- hist(calculated_features$cols_with_3p)
-  aspect_ratio_hist <- hist(calculated_features$aspect_ratio)
+  nr_pix_hist <- hist(calculated_features$nr_pix, main = "Number of black pixels", xlab="Black pixel number", border = "blue", col = "green", las = 1)
+  rows_with_1_hist <- hist(calculated_features$rows_with_1, main = "rows with 1 pixel", xlab = "Rows with 1 number")
+  cols_with_1_hist <- hist(calculated_features$cols_with_1, main = "columns with 1 pixel", xlab = "columns with 1 number")
+  rows_with_3p_hist <- hist(calculated_features$rows_with_3p, main = "rows with 3 pixels or more", xlab = "rows with 3 pixels or more")
+  cols_with_3p_hist <- hist(calculated_features$cols_with_3p, main = "columns with 3 pixels of more", xlab = "columns with 3 pixels or more")
+  aspect_ratio_hist <- hist(calculated_features$aspect_ratio, main = "aspect ratios", xlab = "aspect ratio")
   
   print(nr_pix_hist)
   print(rows_with_1_hist)
